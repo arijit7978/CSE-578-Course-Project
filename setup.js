a9de2c260fdb5d33e0e82bf159cc7105a13f7009
arijit7978 <55776966+arijit7978@users.noreply.github.com>
2020-10-25 13:12:25 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -5,12 +5,13 @@ var ctx = c.getContext("2d");
 var margin = {top: 20, right: 20, bottom: 70, left: 40},
     width = 600 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
-var mapData;
 
 // declare the selecters
 const ist_band = document.querySelector('#Band_1');
 const sec_band = document.querySelector('#Band_2');
 const thi_band = document.querySelector('#Band_3');
+const img_data = document.querySelector('#images');
+var data_mapper;
 
 // This runs when the page is loaded
 document.addEventListener('DOMContentLoaded', function() {
@@ -27,6 +28,12 @@ document.addEventListener('DOMContentLoaded', function() {
     data_2014_12_30 = values[3]; data_2015_02_15 = values[4]; data_2015_06_24 = values[5];
     data_2015_09_12 = values[6]; data_2015_11_15 = values[7]; data_2016_03_06 = values[8];
     data_2016_06_26 = values[9]; data_2016_09_06 = values[10]; data_2016_12_19 = values[11];
+
+    // map input values to variables
+    data_mapper = {'image01_2014_03_17': data_2014_03_17, 'image02_2014_08_24': data_2014_08_24, 'image03_2014_11_28': data_2014_11_28,
+      'image04_2014_12_30': data_2014_12_30, 'image05_2015_02_15': data_2015_02_15, 'image06_2015_06_24': data_2015_06_24,
+      'image07_2015_09_12': data_2015_09_12, 'image08_2015_11_15': data_2015_11_15, 'image09_2016_03_06': data_2016_03_06,
+      'image10_2016_06_26': data_2016_06_26, 'image11_2016_09_06': data_2016_09_06, 'image12_2016_12_19': data_2016_12_19}
     drawMap();
   })
 });
@@ -35,18 +42,19 @@ document.addEventListener('DOMContentLoaded', function() {
 ist_band.addEventListener('change', drawMap);
 sec_band.addEventListener('change', drawMap);
 thi_band.addEventListener('change', drawMap);
+img_data.addEventListener('change', drawMap);
 
 
 function drawMap() {
-  console.log(data_2014_03_17);
+  data = data_mapper[img_data.value]
   var imgdata = ctx.getImageData(0, 0, 651, 651);
   var imgdatalen = imgdata.data.length;
 
   for (var i = 0; i < imgdatalen / 4; i++) { //iterate over every pixel in the canvas
 
-    imgdata.data[4 * i] = data_2014_03_17[i][ist_band.value]; // RED (0-255)
-    imgdata.data[4 * i + 1] = data_2014_03_17[i][sec_band.value]; // GREEN (0-255)
-    imgdata.data[4 * i + 2] = data_2014_03_17[i][thi_band.value]; // BLUE (0-255)
+    imgdata.data[4 * i] = data[i][ist_band.value]; // RED (0-255)
+    imgdata.data[4 * i + 1] = data[i][sec_band.value]; // GREEN (0-255)
+    imgdata.data[4 * i + 2] = data[i][thi_band.value]; // BLUE (0-255)
     imgdata.data[4 * i + 3] = 255; // APLHA (0-255)
   }
   ctx.putImageData(imgdata, 0, 0);
